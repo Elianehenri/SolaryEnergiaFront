58bca5e569b2178238c0c8f207c2d1196aa7a1ee
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-01-19 20:11:10 -0300

    Criar footer e sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { GraficoComponent } from './components/grafico/grafico.component';
+import { CadastroUnidComponent } from './components/cadastro-unid/cadastro-unid.component';
+import { EditarUnidComponent } from './components/editar-unid/editar-unid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    SidebarComponent,
+    GraficoComponent,
+    CadastroUnidComponent,
+    EditarUnidComponent
   ],
   imports: [
     BrowserModule,
